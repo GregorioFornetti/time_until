4b589d2f55fad00f705db1edb4f552ae5e393700
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-03-23 22:04:46 +0100

    Update Home.vue

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+import EventModal from '../modal/EventModal.vue';
 </script>
 
 <template>
-
+    <EventModal
+        type="create"
+        :show="true"
+    />
 </template>
 
 <style scoped>
