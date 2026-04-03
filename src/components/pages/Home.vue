52c8ec9fa27f7ef536915e350c716e67d4a2c827
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-04-03 21:12:51 +0200

    Update Home.vue

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import EventModal from '../modal/EventModal.vue';
+import SuccessAlert from '../SuccessAlert.vue';
+import { i18n } from '../../plugins/i18n'
+
+const showModal = ref(true)
+const showAlert = ref(false)
+
+function closeModal() {
+    showModal.value = false
+}
+
+const triggerAlert = () => {
+  showAlert.value = true
+}
 </script>
 
 <template>
+    <SuccessAlert
+        v-model:show="showAlert"
+        :message="i18n.t('alert.successMessage')"
+        :duration="4000"
+        class="mt-4"
+    />
+
     <EventModal
         type="create"
-        :show="true"
+        old-title="AAAAA"
+        :show="showModal"
+        @update="triggerAlert"
+        :closeModal="closeModal"
     />
 </template>
 
