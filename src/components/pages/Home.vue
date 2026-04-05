349f1987b619594437257bc3f1b850f295321461
Gregório Fornetti Azevedo <fornetti.greg@gmail.com>
2026-04-05 10:54:30 +0200

    Update Home.vue

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -39,42 +39,57 @@ function triggerAlert() {
 
 
 const events = ref<EventInfo[]>(getEventsFromLocalStorage())
-const showModal = ref(true)
+const showModal = ref(false)
 const showAlert = ref(false)
 const title = ref('')
 const description = ref('')
 const startDate = ref('')
 const endDate = ref('')
+const oldTitle = ref('')
 </script>
 
 <template>
+
+    <div class="flex flex-wrap gap-6 mt-8 p-12 justify-center">
+        <EventCard
+            v-for="event of events"
+            :title="event.title"
+            :description="event.description"
+            :startDate="event.startDate"
+            :endDate="event.endDate"
+            @edit="() => {
+              console.log(event.startDate)
+              title = event.title
+              description = event.description || ''
+              startDate = event.startDate || ''
+              endDate = event.endDate || ''
+              oldTitle = event.title
+              showModal = true
+            }"
+        />
+
+        <AddEventCard @click="showModal = true" />
+    </div>
+
+
+
     <SuccessAlert
         v-model:show="showAlert"
         :message="i18n.t('alert.successMessage')"
         :duration="4000"
         class="mt-4"
     />
-
     <EventModal
         v-model:title="title"
         v-model:description="description"
         v-model:startDate="startDate"
         v-model:endDate="endDate"
 
-        old-title="AAAAA"
+        :old-title="oldTitle"
         :show="showModal"
         @update="triggerAlert"
         :closeModal="closeModal"
     />
-
-    <EventCard
-        title="Sample Event asdas ds dasd asd asd asd asdasdas asd asd "
-        description="This is a sample event description to demonstrate the EventCard component. Testando se aparece o bagulho e ai mermaoooo"
-        startDate="2024-07-01"
-        endDate="2024-07-02"
-    />
-
-    <AddEventCard @click="showModal = true" />
 </template>
 
 <style scoped>
